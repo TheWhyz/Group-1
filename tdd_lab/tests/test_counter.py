244a0e98baed891813b004610588cc15ad5514ab
Maylee <maylee.delrio12@gmail.com>
2025-02-05 00:07:52 -0800

    did the test case 7 delete a counter

--- tdd_lab/tests/test_counter.py
+++ tdd_lab/tests/test_counter.py
@@ -10,3 +10,29 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+import pytest
+from src import app
+from src import status
+
+@pytest.fixture()
+def client():
+    """Fixture for Flask test client"""
+    return app.test_client()
+
+@pytest.mark.usefixtures("client")
+class TestCounterEndpoints:
+    """Test cases for Counter API"""
+
+    def test_create_counter(self, client):
+        """It should create a counter"""
+        result = client.post('/counters/foo')
+        assert result.status_code == status.HTTP_201_CREATED
+
+    def test_delete_counter(self, client):
+        """It should delete an existing counter"""
+
+        client.post('/counters/my_counter')
+        response = client.delete('/counters/my_counter')
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        response = client.get('/counters/my_counter')
+        assert response.status_code == status.HTTP_404_NOT_FOUND
